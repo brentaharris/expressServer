2efed8cef96f68cab8689ed13d370a20725f343c
brentaharris <brentaustinharris@gmail.com>
2022-07-23 12:32:02 -0400

    added more routes

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const campsiteRouter = express.Router();
 
+//base routing
 campsiteRouter.route('/')
 .all((req, res, next) => {
     res.statusCode = 200;
@@ -21,4 +22,24 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+// campsiteId routing
+campsiteRouter.route('/:campsiteId')
+.all((req, res) => {
+    res.statusCode = 200;
+    req.setHeader('Content/Type', 'text/plain')
+    res.end(`Will send the details of the campsite ${req.params.campsiteId}`)
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsite/${req.params.campsiteId}`)
+})
+.put((req, res) => {
+    res.end(`Will update the campsite: ${req.body.name} with description ${req.body.description}`)
+})
+.delete((req, res) => {
+    res.end(`Deleting campsite of id: ${res.params.campsiteId}`)
+})
+
+
 module.exports = campsiteRouter;
+
